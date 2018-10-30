ada422a9d2babe2a745b879d8bc132fae874e3e9
Tyler Mendes de Brito <37677424+tylerbryto@users.noreply.github.com>
2018-10-29 23:16:02 -0300

    Update Person.go
    formatting

--- entities/Person.go
+++ entities/Person.go
@@ -1,7 +1,7 @@
 package entities
 
 type Person struct {
-	ID        string   `json:"id,omitempty"`
+    ID        string   `json:"id,omitempty"`
     Firstname string   `json:"firstname,omitempty"`
     Lastname  string   `json:"lastname,omitempty"`
     Address   *Address `json:"address,omitempty"`
